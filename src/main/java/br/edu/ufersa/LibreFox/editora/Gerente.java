074e2e4040bd2fc1d6f202b1487229db0f95fc8a
FelipeVor <felipe.gab.vor1802@gmail.com>
2026-05-29 12:32:49 -0300

    Ported a few classes i missed when porting to maven

---
+++ src/main/java/br/edu/ufersa/LibreFox/editora/Gerente.java
@@ -0,0 +1,7 @@
+package br.edu.ufersa.LibreFox.editora;
+
+public class Gerente extends Usuario{
+    public Gerente (String nome, String cpf, String endereco){
+        super(nome, cpf, endereco);
+    }
+}
